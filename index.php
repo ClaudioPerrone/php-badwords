50859598af33992bf2d2762823e136fa20204867
ClaudioPerrone <cla-agadir97@hotmail.it>
2024-04-30 17:44:42 +0200

    Feat: added the feature for counting the words inside the paragraph

--- index.php
+++ index.php
@@ -1,20 +1,34 @@
+<!-- 
+
+Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
+Gestire il tutto con due file diversi.
+Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.
+Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+- stampare a schermo il paragrafo e la sua lunghezza
+- stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare
+
+ -->
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 <body>
+    <h1>PHP Badwords</h1>
+    
     <form action="saluto.php" method="POST">
         <div>
-            <label>Nome</label>
-            <input name="firstname" type="text" />
+            <label>Paragrafo</label>
+            <textarea placeholder="inserisci il testo del paragrafo" name="paragraph" cols="20" rows="5"></textarea>
         </div>
 
         <div>
-            <label>Cognome</label>
-            <input name="lastname" type="text" />
+            <label>Parola da censurare</label>
+            <input name="badword" type="text" />
         </div>
 
         <div>
--- saluto.php
+++ saluto.php
@@ -1,6 +1,6 @@
 <?php
-$name = $_POST['firstname'];
-$lastname = $_POST['lastname'];
+$paragraph = $_POST['paragraph'];
+$badword = $_POST['badword'];
 ?>
 
 <!DOCTYPE html>
@@ -11,8 +11,21 @@ $lastname = $_POST['lastname'];
     <title>Document</title>
 </head>
 <body>
-    <!-- Alternative -->
-    <h2>Ciao <?php echo $name . ' ' . $lastname; ?></h1>
-    <h2>Ciao <?php echo $name; ?> <?php echo $lastname; ?></h2>
+
+    <div>
+        <h1>IL TESTO E':</h1>
+        <h2><?php echo $paragraph; ?></h2>
+        <h1>IL TESTO E' LUNGO:</h1>
+        <h2><?php echo strlen($paragraph)?></h2>
+    </div>
+
+    <div>
+        <h1>IL TESTO E':</h1>
+        <p><?php echo $str_replace($badword,"***",$paragraph); ?></p>
+    </div>
+
+
+    <h2>Il testo è: <?php echo $badword; ?></h1>
+
 </body>
 </html>
